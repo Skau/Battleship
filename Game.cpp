4be79a7ee4473608b0513943f63a227cbe493223
Skau <kskau93@gmail.com>
2017-11-09 12:07:44 +0100

    Added issues

--- Game.cpp
+++ Game.cpp
@@ -156,29 +156,10 @@ void Game::gameLoop()
     ai.v_Ships.clear();
 }
 
-
-
-
-//the game makes the map with default values
-//9x9 grid where alle squares are empty
-//initializes all variables for both players
-//the player places the ships
-//loops till the player is done
-//the AI places the ships
-//loops till the AI is done
-//player starts
-//enters a combination of a letter and a number (ex: B3)
-//checks if its a legit coordinate on the board
-//if it is:
-//checks to see if it hits a ship
-//if it does mark it as a hit
-//if not mark it as a miss
-
-//if its not legit, ask player to enter coords again
-//when the player is done its the AI turn
-//ai shoots on a square
-//mark if hit or miss
-//loop back to player
-//when all spots where a ship is is marked as a hit the game is over
-//ask to play again
-//loop if so, else quit game
+//ISSUES:
+/*
+ * Need to implement Smart AI (that doesn't just shoot randomly). If AI gets a hit, he should shoot close to the place he hit next round.
+ * Shooting phase needs to be looped. If you shoot somewhere that's already been shot at, you should get another try.
+ * Something is off with the manual placement of ships. Gets error "occupied space", then gives you ships for free. Mostly related to vertical placements.
+ * Game seems to crash when you want to play again, something is not getting initialized correctly.
+*/
